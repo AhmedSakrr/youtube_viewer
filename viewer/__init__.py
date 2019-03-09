50ed5981d84a71e73920abe34d5f5539c0a87eac
Sam Stuver <sam_stuver@bose.com>
2019-03-09 11:44:22 -0700

    disable tracking modifications

--- viewer/__init__.py
+++ viewer/__init__.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '7f98cb734d488bb3caa89a51fdc20154'
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
